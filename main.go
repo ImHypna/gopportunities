cbf9f57c8d9da0aa69991cb467d379e5ee325ff9
Cagol <phcagol2002@gmail.com>
2025-08-22 16:49:56 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"github.com/ImHypna/gopportunities.git/config"
 	r "github.com/ImHypna/gopportunities.git/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
+
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.ErrorF("config initialization error: %v", err)
+		return
+	}
+
 	//Initialize Router
 
 	r.Initialize()
