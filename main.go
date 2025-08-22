a81c696eea6af2c2248b82cae78a8a9bdf355d4c
Cagol <phcagol2002@gmail.com>
2025-08-22 14:17:41 -0300

    feat: add Routes Logic

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	r "github.com/ImHypna/gopportunities.git/router"
+)
+
+func main() {
+	//Initialize Router
+
+	r.Initialize()
+}
---
+++ router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Initialize() {
+	//initialize router
+	router := gin.Default()
+	//initialize Routes
+	initializeRoutes(router)
+
+	//run the server
+	router.Run(":8080")
+}
